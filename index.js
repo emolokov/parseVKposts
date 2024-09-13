eb862671b9bc8c5720b996aea272eb9f7f859959
egormolokov <e92molokov@gmail.com>
2024-09-13 23:01:22 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import * as VKID from '@vkid/sdk';
 
+const client_id = 52227698
+const scope = "groups friends"
+const redirect_uri = "http://localhost"
+
+VKID.Config.init({
+    app: client_id, // Идентификатор приложения.
+    redirectUrl: redirect_uri, // Адрес для перехода после авторизации.
+    scope: scope, // Список прав доступа, которые нужны приложению.
+    mode: VKID.ConfigAuthMode.InNewTab // По умолчанию авторизация открывается в новой вкладке.
+});
 
 
 
