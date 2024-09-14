c824ee87f7c5b47b886eea5a3cf504d7fd955d2b
egormolokov <e92molokov@gmail.com>
2024-09-14 11:55:29 +0300

    start

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+
+// const client_id = 52227698
+// const scope = "groups friends"
+// const redirect_uri = "http://localhost"
+
+const app = express()
+
+
+async function main() {
+    app.use(express.json());
+
+    app.get('/', (req, res) => {
+        res
+            .json({
+                message: 'success'
+            }).status(200)
+    })
+
+    app.listen(5683, () => {
+        console.log('Server is running on port 5683')
+    })
+
+
+    // VKID.Config.init({
+    //     app: client_id, // Идентификатор приложения.
+    //     redirectUrl: redirect_uri, // Адрес для перехода после авторизации.
+    //     scope: scope, // Список прав доступа, которые нужны приложению.
+    //     mode: VKID.ConfigAuthMode.InNewTab // По умолчанию авторизация открывается в новой вкладке.
+    // });
+
+}
+
+main()
+
